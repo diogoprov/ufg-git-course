8a9e07257feaf338c73a79edf89d298d5e2460c1
LucianoFSgarbi <luciano.f.sgarbi@gmail.com>
2014-02-26 19:50:07 -0300

    modifiquei o codigo do diogo

--- plot_function.R
+++ plot_function.R
@@ -1,12 +1,12 @@
 # Define 2 vectors
-cars <- c(1, 3, 6, 4, 9)
-trucks <- c(2, 5, 4, 5, 12)
+carros <- c(1, 3, 6, 4, 9)### mudei o nome
+caminhoes <- c(2, 5, 4, 5, 12)### mudei o nome
 
 # Graph cars using a y axis that ranges from 0 to 12
-plot(cars, type="o", col="red", ylim=c(0,12))
+plot(carros, type="o", col="red", ylim=c(0,12))
 
 # Graph trucks with red dashed line and square points
-lines(trucks, type="o", pch=22, lty=2, col="red")
+lines(caminhoes, type="o", pch=22, lty=2, col="red")
 
 # Create a title with a red, bold/italic font
-title(main="Autos", col.main="red", font.main=4)
+title(main="Automóveis", col.main="red", font.main=4)
